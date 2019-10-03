ad57546df083d95e8e205729f5a72c9b274ee36d
Jasper <gogs@fake.local>
2019-10-03 17:05:58 +0800

    example 3 finished

--- examples/example_3/app.js
+++ examples/example_3/app.js
@@ -1,278 +1,263 @@
-function createNode({ classes = "", html = "", link = "" }) {
+const mobile_breakpoint="sm";
+
+function createNode({ classes = "",photo_url="",name="",title="",tel="",email="",duties_link=""  }) {
+  classes+=` p-3 p-${mobile_breakpoint}-0 myDefaultNode`;
+  const html=`
+  <div class="myDesktopNode d-none d-${mobile_breakpoint}-flex flex-column justify-content-center bg-transparent">
+    ${photo_url?`<img class="photo img-fluid" src="${photo_url}">`:''}
+      <div>
+        <span class="name">${name}</span><br>
+        ${duties_link?`<a class="title" href="${duties_link}">${title}</a>`:`<span class="title">${title}</span>`}<br>
+        <i class="fas fa-phone tel-outer">
+        <span class="tel-inner"><a class="tel" href="tel:${tel}">Tel:&nbsp;${tel}</a></span>
+        </i>
+        <i class="v-line"></i>
+        <i class="far fa-envelope email-outer">
+        <span class="email-inner"><a class="email" href="mailto:${email}">Email:&nbsp;${email}</a></span>
+        </i>
+        ${duties_link?`<i class="v-line"></i><a class="duties" href="${duties_link}">Duties</a>`:''}
+      </div>
+  </div>
+  <div class="myMobileNode d-flex d-${mobile_breakpoint}-none">
+    ${photo_url?`<img class="photo img-fluid mr-3" src="${photo_url}">`:''}
+    <div>
+      <span class="name font-weight-bold">${name}</span><br><br>
+      ${duties_link?`<a class="title" href="${duties_link}">${title}</a>`:`<span class="title">${title}</span>`}<br>
+      <a class="tel" href="tel:${tel}">Tel:&nbsp;${tel}</a><br>
+      <a class="email" href="mailto:${email}">Email:&nbsp;${email}</a>
+    </div>
+  </div>
+  `;
   return {
     classes,
-    html,
-    link
+    html
   };
 }
 
-const commissioner = createNode({
-  classes: "area1",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area1",
-  html: `
-        <div class="d-flex title">
-            <img src="./android19.png" style="max-height:100px;flex-basis:25%" class="img-fluid mr-3" ></img>
-            <div>
-              Commissioner for Labour
-            </div>
-        </div>
-        <div class="person">
-            Mr C Chan<br>
-            Tel: 2852 3688
-        </div>        
-        `
+function createNoBody() {
+  return {
+    classes: "myNoBodyNode nobody_line"
+  };
+}
+
+const gcio=createNode({
+  classes:'gcio',
+  photo_url:'gcio.jpg',
+  name:'Mr. Victor Lam, JP',
+  title:'Government Chief Informaiton Officer',
+  tel:'2810 2611',
+  email:'vlam@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/gcio.html'
 });
 
-const deputy_commissioner_la = createNode({
-  classes: "area2",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area2",
-  html: `
-        <div class="title">Deputy Commissioner<br>(Labour Administration)</div>
-        <div class="person">Miss M Li<br>
-        Tel: 2852 4093</div>
-        `
-});
-const deputy_commissioner_osh = createNode({
-  classes: "area2",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area2",
-  html: `
-        <div class="title">Deputy Commissioner<br>(Occupational Safety and Health)</div>
-        <div class="person">Mr J Leung<br>
-        Tel: 2852 4192</div>
-        `
+const dgcio=createNode({
+  classes:'dgcio',
+  name:'Miss Donna Chan',
+  title:'Deputy Government Chief Information Officer',
+  tel:'2810 2621',
+  email : 'donchan@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/dgcio.html'
 });
 
-const assistant_commissioner_lr = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Assistant Commissioner<br>(Labour Relations)</div>
-        <div class="person">Ms M Luk<br>
-        Tel: 2852 4099</div>
-        `
+const agcio_csd=createNode({
+  classes:'agcio',
+  name:'Mr. Jason Pun',
+  title:'Assistant Government Chief Information Officer (Cyber Security & Digital Identity)',
+  tel: '2810 2623',
+  email : 'jskpun@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/agcio_csd.html'
 });
 
-const assistant_commissioner_es = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Assistant Commissioner<br>(Employment Services)</div>
-        <div class="person">Mr C Hui<br>
-        Tel: 2852 4102</div>
-        `
+const csm_sec=createNode({
+  classes:'csm',
+  name:'Mr. Tony Wong',
+  title:'Chief Systems Manager (Cyber Security)',
+  tel: '2867 2036',
+  email : 'tkmwong@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_sec.html'
 });
 
-const assistant_commissioner_erb = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Assistant Commissioner<br>(Employment's Rights and Benefits)</div>
-        <div class="person">Mr S Li<br>
-        Tel: 2852 4083</div>
-        `
+const csm_di=createNode({
+  classes:'csm',
+  name:'Mr. Simon Siu',
+  title:'Chief Systems Manager (Digital Identity)',
+  tel: '2867 4484',
+  email : 'skwsiu@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_di.html'
 });
 
-const assistant_commissioner_ps = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Assistant Commissioner<br>(Policy Support)</div>
-        <div class="person">Miss H S Cheung<br>
-        Tel: 2852 3633</div>
-        `
+const agcio_inf=createNode({
+  classes:'agcio',
+  name:'Mr. Donald Mak',
+  title:'Assistant Government Chief Information Officer (IT Infrastructure)',
+  tel: '2810 2626',
+  email : 'dckmak@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/agcio_inf.html'
 });
 
-const assistant_commissioner_d = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Assistant Commissioner<br>(Development)</div>
-        <div class="person">Mr R Ho<br>
-        Tel: 2852 4148</div>
-        `
+const csm_inf=createNode({
+  classes:'csm',
+  name:'Mr. KH Poon',
+  title:'Chief Systems Manager (IT Infrastructure)',
+  tel: '3182 6516',
+  email : 'khpoon@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_inf.html'
 });
 
-const assistant_commissioner_os = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Assistant Commissioner<br>(Occupational Safety)</div>
-        <div class="person">Mr W H Wu<br>
-        Tel: 2852 4070</div>
-        `
+const csm_da=createNode({
+  classes:'csm',
+  name:'Mr. Anthony Chiu',
+  title:'Chief Systems Manager (Data Analytics)',
+  tel: '2867 4482',
+  email : 'ashchiu@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_da.html'
 });
 
-const occupational_health_consultant_1 = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Occupational Health Consultant(1)</div>
-        <div class="person">Dr M Y Ho<br>
-        Tel: 2852 4183</div>
-        `
+const agcio_gr=createNode({
+  classes:'agcio',
+  name:'Mr. Kingsley Wong',
+  title:'Assistant Government Chief Information Officer (Governance & Resources)',
+  tel: '2810 2090',
+  email : 'kkmwong@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/agcio_gr.html'
 });
 
-const occupational_health_consultant_2 = createNode({
-  classes: "area3",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area3",
-  html: `
-        <div class="title">Occupational Health Consultant(2)</div>
-        <div class="person">Dr Y K Kwan<br>
-        Tel: 3543 5713</div>
-        `
+const csm_gr=createNode({
+  classes:'csm',
+  name:'Mr. Gavin Wah',
+  title:'Chief Systems Manager (Governance & Resources)',
+  tel: '2867 2628',
+  email : 'gkkwah@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_gr.html'
 });
 
-const nobody1=createNode({classes:"nobody_line",html:'<div style="height:8em"></div>'});
-
-const chief_labour_officer_lr = createNode({
-  classes: "area4",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area4",
-  html: `
-        <div class="title">Chief Labour Officer<br>(Labour Relations)</div>
-        <div class="person">Mr R Liang<br>
-        Tel: 2852 3457</div>
-        `
+const csm_io=createNode({
+  classes:'csm',
+  name:'Mr. Patrick Chan',
+  title:'Chief Systems Manager (IT Operations)',
+  tel: '2867 4483',
+  email : 'pkkchan@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_io.html'
 });
 
-const chief_labour_officer_op = createNode({
-  classes: "area4",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area4",
-  html: `
-        <div class="title">Chief Labour Officer<br>(Offsetting Policy)</div>
-        <div class="person">Miss G Chan<br>
-        Tel: 2852 3553</div>
-        `
+const agcio_id=createNode({
+  classes:'agcio',
+  name:'Mr. Tony Wong, JP',
+  title:'Assistant Government Chief Information Officer (Industry Development)',
+  tel: '2810 2610',
+  email : 'tonywong@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/agcio_id.html'
 });
 
-const chief_labour_officer_whp = createNode({
-  classes: "area4",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area4",
-  html: `
-        <div class="title">Chief Labour Officer<br>(Working Hours Policy)</div>
-        <div class="person">Mr R Leung<br>
-        Tel: 3586 8006</div>
-        `
+const csm_id=createNode({
+  classes:'csm',
+  name:'Ms. Chrissie Ng',
+  title:'Chief Systems Manager (Industry Development)',
+  tel: '2867 2422',
+  email : 'cskng@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_id.html'
 });
 
-const chief_labour_officer_smw = createNode({
-  classes: "area4",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area4",
-  html: `
-        <div class="title">Chief Labour Officer<br>(Statutory Minimum Wage)<br>Registrar of Trade Unions</div>
-        <div class="person">Ms B Ng<br>
-        Tel: 2852 3842</div>
-        `
+const csm_sc=createNode({
+  classes:'csm',
+  name:'Mr. Rex Tong',
+  title:'Chief Systems Manager (Smart City)',
+  tel: '2867 4488',
+  email : 'rtong@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_sc.html'
 });
 
-const chief_occupational_safety_officer_o = createNode({
-  classes: "area4",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area4",
-  html: `
-        <div class="title">Chief Occupational_Safety_Officer<br>(Operations)</div>
-        <div class="person">Mr K L Chan<br>
-        Tel: 2852 4176</div>
-        `
+const nobody1=createNoBody();
+
+const csm_cs=createNode({
+  classes:'csm',
+  name:'Mr. Daniel Cheung',
+  title:'Chief Systems Manager (Common Services)',
+  tel: '2867 4608',
+  email : 'dywcheung@ogcio.gov.hk',
+  duties_link:'https://www.ogcio.gov.hk/en/about_us/org_chart/csm_cs.html'
 });
 
-
-const chief_occupational_safety_officer_ss = createNode({
-  classes: "area4",
-  link: "https://www.labour.gov.hk/eng/organ/programmen.htm#area4",
-  html: `
-        <div class="title">Chief Occupational_Safety_Officer<br>(System and Support)</div>
-        <div class="person">Mr C P Wan<br>
-        Tel: 2852 4963</div>
-        `
+const cso_ad=createNode({
+  classes:'cso',
+  name:'Ms. Fiona Li',
+  title:'Chief Executive Officer (Administration Division)',
+  tel: '2867 5378',
+  email : 'fmwli@ogcio.gov.hk'
 });
 
+const sta_fd=createNode({
+  classes:'sta',
+  name:'Ms. Catherine Chu',
+  title:'Senior Treasury Accountant (Finance Division)',
+  tel: '2582 4493',
+  email : 'cspchu@ogcio.gov.hk'
+});
 
 const data = {
   orgChart_data: {
-    mobile_breakpoint: "sm",
+    mobile_breakpoint: mobile_breakpoint,
     connector_color:"black",
     root: {
-      node: commissioner,
-      branches: {
-        items: [
-          {
-            node: deputy_commissioner_la,
-            branches: {
-              items: [
-                {
-                  node: assistant_commissioner_lr,
-                  branches:{
-                    vertical_breakpoint:"lg",
-                    items:[
-                      {
-                        node:chief_labour_officer_lr
-                      },
-                      {
-                        node:chief_labour_officer_op
-                      }
-                    ]
-                  }
-                },
-                {
-                  node:assistant_commissioner_es
-                },
-                {
-                  node:assistant_commissioner_erb
-                },
-                {
-                  node:assistant_commissioner_ps,
-                  branches:{
-                    items:[
-                      {
-                        node:chief_labour_officer_whp
-                      }
-                    ]
-                  }
-                },
-                {
-                  node:assistant_commissioner_d,
-                  branches:{
-                    items:[
-                      {
-                        node:chief_labour_officer_smw
-                      }
-                    ]
-                  }
-                }
-              ]
-            }
-          },
-          { node: deputy_commissioner_osh,
+      node:gcio,
+      branches:{
+        items:[
+          {node:dgcio,
           branches:{
             items:[
               {
-                node:assistant_commissioner_os,
+                node:agcio_csd,                
                 branches:{
+                  vertical_breakpoint: "*",
                   items:[
-                    {
-                      node:chief_occupational_safety_officer_o
-                    }
+                    {node:csm_sec},
+                    {node:csm_di}
+                  ]
+                }
+              },
+              {
+                node:agcio_inf,                
+                branches:{
+                  vertical_breakpoint: "*",
+                  items:[
+                    {node:csm_inf},
+                    {node:csm_da}
+                  ]
+                }
+              },
+              {
+                node:agcio_gr,                
+                branches:{
+                  vertical_breakpoint: "*",
+                  items:[
+                    {node:csm_gr},
+                    {node:csm_io}
+                  ]
+                }
+              },
+              {
+                node:agcio_id,                
+                branches:{
+                  vertical_breakpoint: "*",
+                  items:[
+                    {node:csm_id},
+                    {node:csm_sc}
                   ]
                 }
               },
               {
                 node:nobody1,
                 branches:{
+                  vertical_breakpoint: "*",
                   items:[
-                    {
-                      node:chief_occupational_safety_officer_ss
-                    }
-                  ]              
+                    {node:csm_cs},
+                    {node:cso_ad},
+                    {node:sta_fd}
+                  ]
                 }
-              },
-              {
-                node:occupational_health_consultant_1
-              },
-              {
-                node:occupational_health_consultant_2
               }
             ]
-          }
-          }
+          }}
         ]
       }
     }
@@ -283,3 +268,5 @@ const app = new Vue({
   el: "#app",
   data
 });
+
+
